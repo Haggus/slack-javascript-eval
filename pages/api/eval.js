f2a692254fece942f34b28f412924922b86b2945
Mateusz Mrowiec <matt.mrowiec@gmail.com>
2019-09-29 15:59:26 +0200

    Add check for empty body

--- pages/api/eval.js
+++ pages/api/eval.js
@@ -1,6 +1,13 @@
 import evaluateIncomingJS from '../../lib/eval';
 
 export default async (req, res) => {
+  const response_type = 'in_channel';
+
+  if (!req.body.text) {
+    res.setHeader('Content-Type', 'application/json');
+    return res.json({ response_type, text: 'Error: Missing source code to evaluate' });
+  }
+
   let result, attachments;
 
   try {
@@ -8,12 +15,11 @@ export default async (req, res) => {
     result = evaluateIncomingJS(req.body.text, 2500);
   } catch (error) {
     // Capture any errors
-    result = error.message;
+    result = 'Error: ' + error.message;
     attachments = [{ text: error.stack }];
   }
 
   const message = '`' + req.body.text + '` ```' + result + '```';
-  const response_type = 'in_channel';
 
   res.setHeader('Content-Type', 'application/json');
   // Create response object and send result back to Slack
